cd0c7027088759e7c826c597c81f793d1a2fe6da
nathnael1 <nathnaeltamirat3@gmail.com>
2023-11-10 09:59:04 -0900

    added

---
+++ site/Web design for everybody capstone/Week 6/Scratch/scratch mobile.js
@@ -0,0 +1,13 @@
+function wating(){
+const change=document.getElementById("active");
+const ochange=document.getElementById("first");
+    change.innerHTML="Freshman Course other-coming soon";
+    ochange.innerHTML="Freshman Course other-coming soon";
+}
+function leaving(){
+    const change=document.getElementById("active");
+    const ochange=document.getElementById("first");
+        change.innerHTML="Freshman Course (NS) -1";
+        ochange.innerHTML="Freshman Course (NS) -1";
+    }
+
---
+++ site/Web design for everybody capstone/Week 6/Template/script.js
@@ -0,0 +1,10 @@
+const change=document.getElementById("fresh");
+function first(){
+window.alert("COMING SOON");
+}
+function second(){
+change.innerHTML="COMING SOON";
+}
+function third(){
+    change.innerHTML="Freshman Course (NS) - 1";
+}
